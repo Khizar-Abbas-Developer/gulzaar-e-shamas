34ad67e6c96718d1a66c2b0ee61559615392fdb3
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-08-08 01:31:47 +0500

    4fdfffd

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Form } from "../ui/form";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
