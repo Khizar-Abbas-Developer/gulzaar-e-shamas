4d5da77f120710edf73d5bfe8157477de75a4489
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-09-09 00:17:22 +0500

    45454

--- components/HomeButtons.jsx
+++ components/HomeButtons.jsx
@@ -34,17 +34,14 @@ const HomeButtons = () => {
           <Link href="/user/register" className="flex justify-start">
             <Button
               type="submit"
-              disabled={isLoading}
+              variant="yellow" // Now this works
               className={
                 "shad-primary-btn w-[50%] md:w-full mx-auto flex flex-col gap-2 px-4 bg-[#1f9367] text-white md:gap-4 md:py-12 md:px-6 hover:bg-[#4d9076]"
               }
               style={{ paddingTop: "40px", paddingBottom: "40px" }}
-              onClick={() => {}}
             >
-              <p className="text-base md:text-xl font-urdu">
-                شجرے کا اندراج کروائیں
-              </p>
-              <p className="text-base md:text-xl">Register the family tree</p>{" "}
+              <p className="text-base md:text-xl font-urdu">ابھی کتاب خریدیں</p>
+              <p className="text-base md:text-xl">Buy Book Now</p>
             </Button>
           </Link>
         </form>
